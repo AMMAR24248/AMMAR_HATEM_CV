7974a7075869c6125fbabc4c5679d81ac5504c7c
AMMAR HATEM ALWAN <162059663+AMMAR24248@users.noreply.github.com>
2026-07-09 10:34:50 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-console.log("Portfolio script loaded");
+const sections = document.querySelectorAll(".section");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    entries.forEach(function (entry) {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.15,
+  }
+);
+
+sections.forEach(function (section) {
+  observer.observe(section);
+});
+
+const projectCards = document.querySelectorAll(".project-card");
+
+projectCards.forEach(function (card) {
+  card.addEventListener("mousemove", function (event) {
+    const rect = card.getBoundingClientRect();
+
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    card.style.setProperty("--x", x + "px");
+    card.style.setProperty("--y", y + "px");
+  });
+});
